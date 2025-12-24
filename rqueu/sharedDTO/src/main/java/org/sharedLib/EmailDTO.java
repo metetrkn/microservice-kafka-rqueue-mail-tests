78a71aa4266bce5d6fc4dc82e26335cd945c386a
metetrkn <metetrkn52@gmail.com>
2025-12-24 21:18:53 +0100

    Update project package Refactor: kafkaConsumer and kafkaProducer into kafka/ dir Refactor: rqueuConsumer and rqueuProducer into rqueu/ dir Add: sharedDTO project, shared DTO of rqeueuConsumer and rqueuProducer moved here

---
+++ rqueu/sharedDTO/src/main/java/org/sharedLib/EmailDTO.java
@@ -0,0 +1,18 @@
+package org.sharedLib;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+import java.util.Date;
+
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+public class EmailDTO {
+    private String from;
+    private String to;
+    private String subject;
+    private String body;
+    private Date createdAt = new Date();
+}
